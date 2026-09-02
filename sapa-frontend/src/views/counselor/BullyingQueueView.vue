f8b933fc6198a02890e9520854feca4d6096ec93
dikri <dikri@gmail.com>
2026-09-02 11:24:21 +0700

    view page

---
+++ sapa-frontend/src/views/counselor/BullyingQueueView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Bullying Queue</h1>
+</template>
---
+++ sapa-frontend/src/views/errors/UnauthorizedView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Unauthorized</h1>
+</template>
